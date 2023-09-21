938986c6c135f2b379bac9d2cbc76c0a20e9d6f3
Fabrizio Fasanando <fabriziofasanando2@gmail.com>
2023-09-21 19:30:12 +0200

    REFACTOR: format code

--- php/src/RockPaperScissors.php
+++ php/src/RockPaperScissors.php
@@ -1,8 +1,10 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 namespace Kata;
 
-enum ResultRockPaperScissors {
+enum ResultRockPaperScissors
+{
     case PLAYER_1_WINS;
     case PLAYER_2_WINS;
     case DRAW;
