be8a298f91a072d58a09c077041a99b52d993155
katarn <katarn@katarn.es>
2023-09-21 18:34:06 +0200

    test_player2_rock_should_beat_player1_scissors

--- php/tests/RockPaperScissorsTest.php
+++ php/tests/RockPaperScissorsTest.php
@@ -7,12 +7,21 @@ use PHPUnit\Framework\TestCase;
 
 class RockPaperScissorsTest extends TestCase
 {
-    public function test_rock_should_beat_scissors(): void
+    public function test_player1_rock_should_beat_player2_scissors(): void
     {
         $rockPaperScissors = new RockPaperScissors();
 
         $result = $rockPaperScissors->play('rock', 'scissors');
 
         self::assertSame('player1 wins', $result);
+    }
+
+    public function test_player2_rock_should_beat_player1_scissors(): void
+    {
+        $rockPaperScissors = new RockPaperScissors();
+
+        $result = $rockPaperScissors->play('scissors', 'rock');
+
+        self::assertSame('player2 wins', $result);
     }
 }
